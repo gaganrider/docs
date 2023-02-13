e60ca7cfe1502f36716fbb3dd5eed8b8590a7d0a
gaganrider <114590365+gaganrider@users.noreply.github.com>
2023-02-13 15:33:20 +0530

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,28 +1,33 @@
 
-
 // npm i mongoose
+// make folder models
+// create file db.js and userModel.js
 // the term Student is our model name and can be changed 
+
+
+// write in db.js
 // import
 const mongoose =require ('mongoose')
 
 // connect to database
-mongoose.connect("mongodb://localhost:27017/student")// student is the name of the database
+mongoose.connect("mongodb://localhost:27017/student")// student is the name of the database if it finds it then it will connects with it otherwise it will create it
 .then(()=>console.log('mongooseconnected'))// confirms that the database is connected
 .catch((err)=>console.log(err))//throws error if any
 
 
-//creating schema in different directory
+//creating schema in userModel.js
 
 // import
 const mongoose =require ('mongoose');
 
 // creating Schema
+// schema is a formate in which we want out data to be saved
 
 const studentSchema =new mongoose.Schema({
     name:{
         type:String, //type of input
         require:true,//to make it require
-        unique:true,//to make sure the value is unique and not get repeted like email etc
+        unique:true,//to make sure the value is unique and not get repeted like email phone number etc
         trim:true,// to trim the spaces from the values
         default:"defaultvalue"// to set the default value 
     }
@@ -45,7 +50,7 @@ require('./models/db');// replace the path where the database is created
 
 //write in index.js
 // importing usermodel 
-const Student = require('path')//replace path with actual path
+const Student = require('../models/userModel')//replace path with actual path
 //crud operations for mongoose
 
 // creating the data into database
@@ -58,7 +63,7 @@ student.save()
 //orrr
 //when using passport local create stretagy
 const {name ,username,email,password}=req.body;
-Student.register({ name, username,email}, password)
+Student.register({ name, username, email, }, password)
 
 //in updation and deletion we have to find the document which is same as reading it the first argument is the filter which finds the data which is id email or username in most cases
 

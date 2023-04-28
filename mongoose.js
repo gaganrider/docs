57eb8228f6086628ef5a108e488f3efce9b0ab6b
gaganrider <114590365+gaganrider@users.noreply.github.com>
2023-04-28 22:18:10 +0530

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -2,41 +2,19 @@
 // npm i mongoose
 // make folder models
 // create file db.js and userModel.js
-// the term Student is our model name and can be changed 
+
 
 
 // write in db.js
 // import
 const mongoose =require ('mongoose')
 
-// connect to database
-mongoose.connect("mongodb://localhost:27017/student")// student is the name of the database if it finds it then it will connects with it otherwise it will create it
+// CONNECT TO DATABASE
+
+mongoose.connect("mongodb://localhost:27017/databasename")//if it finds the database then it will connects with it otherwise it will create it 
+//path should be a string you can find it on mongodbcompass or mongodb atles
 .then(()=>console.log('mongooseconnected'))// confirms that the database is connected
 .catch((err)=>console.log(err))//throws error if any
-
-
-//creating schema in userModel.js
-
-// import
-const mongoose =require ('mongoose');
-
-// creating Schema
-// schema is a formate in which we want out data to be saved
-
-const studentSchema =new mongoose.Schema({
-    name:{
-        type:String, //type of input
-        require:true,//to make it require
-        unique:true,//to make sure the value is unique and not get repeted like email phone number etc
-        trim:true,// to trim the spaces from the values
-        default:"defaultvalue"// to set the default value 
-    }
-})
-
-// export schema creating model
-const Student = mongoose.model("Student",studentSchema) //try to keep it in CAPS and in singuler form (do not put s in the end)
-
-module.exports = Student;
 
 
 //write in app.js 
@@ -48,51 +26,105 @@ require('./models/db');// replace the path where the database is created
 
 
 
+
+//CREATING SCHEMA in usermodel.js
+
+// import
+const mongoose =require ('mongoose');
+
+// creating Schema
+// schema is a formate in which we want out data to be saved
+
+const userSchema =new mongoose.Schema({
+    name:{
+        type:String, //type of input
+        required:true,//to make it a mendetory field
+        unique:true,//to make sure the value is unique and not get repeted like email phone number etc
+        trim:true,// to trim the spaces from the values
+        default:"defaultvalue"// to set the default value 
+    }
+})
+
+// export schema creating model
+const User = mongoose.model("User",userSchema) //try to keep it in CAPS and in singuler form (do not put s in the end)
+
+module.exports = User;
+
+
+
+
+
+
 //write in index.js
 // importing usermodel 
-const Student = require('../models/userModel')//replace path with actual path
+const User = require('../models/userModel')//replace path with actual path
 //crud operations for mongoose
 
-// creating the data into database
+// CREATING THE DATA 
 
-const student = new Student();
+const user = new User();
 student.name=req.body.name;
 student.username=req.body.username
 student.save()
 
+// orrrrrr
+
+User.create(data)// data should be an object
+
+
 //orrr
 //when using passport local create stretagy
 const {name ,username,email,password}=req.body;
-Student.register({ name, username, email, }, password)
+User.register({ name, username, email, }, password)
 
-//in updation and deletion we have to find the document which is same as reading it the first argument is the filter which finds the data which is id email or username in most cases
-
-//serching or reading data
-Student.findOneAndUpdate({username: "john" }, //will find the data with username john in the database you can change the value or finding perameter like id and username(name k jaga id ya username likhdo) leaving the { } empty will show the whole database
-     null, function (err, docs) {
-    if (err){
-        console.log(err)
-    }
-    else{
-        console.log("Original Doc : ",docs);
-    }
-});
+//GETTING THE DATA
 
 
 
-// updating the data
+//getting all of the data
 
-Student.findOneAndUpdate({username: "john" }, //will find the data with username john in the database you can change the value or finding perameter like id and username(name k jaga id ya username likhdo)
-    {name:"Anuj"}, null, function (err, docs) {// will update the name to anuj docs will contain the details of the user
-    if (err){
-        console.log(err)
-    }
-    else{
-        console.log("Original Doc : ",docs);
-    }
-});
+const data = User.find()// //will find all the data present in our database
+ //data will be an array having our data
+
+// getting the data by its unique id
+
+const data = User.findById(userid)// //will find the data having the given id 
+//since the id is going to unique the data will be an object having our data
 
 
+// getting the data with a different field 
 
-// deleting
-Student.findOneAndDelete({_id:"given id"},(err, docs)=>{})//will find by the id and delete it. (use req.user.id or req.body.id)
+const data = User.findOne({name:'gagan'})// //will find the first data with name gagan
+ //data will be an object having our data
+
+
+const data = User.find({name:'gagan'})// //will find all the data with name gagan, leave it empty to get all the data
+ //data will be an array having our data
+
+ 
+
+
+
+
+// UPDATING THE DATA
+
+//updating data using unique id
+
+const data=User.findByIdAndUpdate(userid, //will find the data with given id in the database 
+    {name:"Anuj"}// will update the name to anuj 
+    );
+// data will hold the updated data
+
+
+//updating data using other parameter
+
+const data=User.findOneAndUpdate({username: "john" }, //will find the data with username john in the database you can change the value or finding perameter 
+    {name:"Anuj"}// will update the name to anuj 
+    );
+// data will hold the updated data
+
+
+
+
+// DELETING THE DATA
+User.findOneAndDelete({_id:"given id"})//will find by the id and delete it. (use req.user.id or req.body.id)
